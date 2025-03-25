74b4846ff29ac069dd478d6759f974fdd97e2931
Sebastian Buritica <jhoseburitica40@gmail.com>
2025-03-25 10:54:38 -0500

    Script de data listo y base de datos llena

---
+++ Models/City.js
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+
+
+
+const collection = "City";
+
+const schema = new Schema(
+  {
+    name: { type: String, required: true },
+    country: { type: String, required: true },
+    language: { type: String, required: true },
+    climate: { type: String, required: true }, // Clima promedio (ej. "Tropical", "Mediterráneo", etc.)
+    currency: { type: String, required: true }, // Moneda local
+    rating: { type: Number, default: 0 }, // Calificación promedio de turistas (de 1 a 5)
+    imageUrl: { type: String, required: true },
+    bestTimeToVisit: { type: String, required: true }, // Mejor época para visitar
+  },
+  {
+    timestamps:true
+  }
+);
+
+const City = model(collection,schema)
+
+
+export default City
--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-console.log("Hola mundo")
+console.clear()
+import express from "express";
+import 'dotenv/config.js'
+import morgan from "morgan";
+import cors from "cors"
+
+const server = express()
+
+
+
+const port = process.env.PORT  || 8080
+
+
+server.use(morgan("dev"))
+server.use(cors())
+
+server.get("/", (req, res) => {
+    res.send("Te escuche baby");
+});
+
+
+
+server.listen(port, ()=>{
+    console.log(`Ya estoy esperando solicitues en el puerto ${port}`)
+})
+
